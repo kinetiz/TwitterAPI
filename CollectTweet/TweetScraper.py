f73dd34ea45762307e585d61120162c6154dda13
kinetiz <thanadon.f@gmail.com>
2018-05-22 02:51:53 +0100

    add more tweet fields to collect

--- CollectTweet/TweetScraper.py
+++ CollectTweet/TweetScraper.py
@@ -6,27 +6,35 @@ import datetime
 # Configuration
 ##############################################################
 ### Setup logging
-logging.basicConfig(filename='historyV1.log',level=logging.INFO, format='%(asctime)s.%(msecs)03d : %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
-logging.info("Save page: " + str(1))
+logging.basicConfig(filename='history.log',level=logging.INFO, format='%(asctime)s.%(msecs)03d : %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+
+### Collecting date range
+collectingDataSince = "2018-05-12"
+collectingDataUntil = "2018-05-16"
 
 ### intial json file name
-filename = 'top1_10_2018_05_16_to_18coins.json'
+filename = 'top1_'+collectingDataSince+'_to_'+collectingDataUntil+'.json'
+
+### save every N page
+bulkSize = 50
 
 ### Define query
 coinTags = {}
-### 1-10 ###
+### 1 ###
 coinTags['bitcoin'] = "#bitcoin OR #btc"
-coinTags['ethereum'] = "#ethereum OR #eth OR #ether"
-coinTags['ripple'] = "#ripple OR #xrp"
-coinTags['bitcoincash'] = "#bitcoincash OR #bch"
-coinTags['eosio'] = "#eosio OR #eos"
-coinTags['litecoin'] = "#litecoin OR #ltc"
-coinTags['cardano'] = "#cardano OR #ada"
-coinTags['stellar'] = "#stellar OR #xlm"
-coinTags['iota'] = "#iota OR #miota"
-coinTags['tron'] = "#tron OR #trx"
 
-# ## 11-20 ###
+### 2-10 ###
+# coinTags['ethereum'] = "#ethereum OR #eth OR #ether"
+# coinTags['ripple'] = "#ripple OR #xrp"
+# coinTags['bitcoincash'] = "#bitcoincash OR #bch"
+# coinTags['eosio'] = "#eosio OR #eos"
+
+# ## 11-30 ###
+# coinTags['litecoin'] = "#litecoin OR #ltc"
+# coinTags['cardano'] = "#cardano OR #ada"
+# coinTags['stellar'] = "#stellar OR #xlm"
+# coinTags['iota'] = "#iota OR #miota"
+# coinTags['tron'] = "#tron OR #trx"
 # coinTags['neo'] = "#neo"
 # coinTags['monero'] = "#monero OR #xmr"
 # coinTags['dash'] = "#dash"
@@ -37,26 +45,25 @@ coinTags['tron'] = "#tron OR #trx"
 # coinTags['bytecoin'] = "#bytecoin OR #bcn"
 # coinTags['binance_coin'] = "#binancecoin OR #bnb"
 # coinTags['qtum'] = "#qtum"
-
-### 21-30 ###
 # coinTags['zcash'] = "#zcash OR #zec"
 # coinTags['icon'] = "#icx"
 # coinTags['omisego'] = "#omisego"
 # coinTags['lisk'] = "#lisk OR #lsk"
 # coinTags['zilliqa'] = "#zilliqa OR #zil"
-# coinTags['bitcoingold'] = "#btg"
+# coinTags['bitcoingold'] = "#bitcoingold OR #btg"
 # coinTags['aeternity'] = "#aeternity OR #ae"
 # coinTags['ontology'] = "#ontology OR #ont"
 # coinTags['verge'] = "#verge OR #xvg"
 # coinTags['steem'] = "#steem"
 
+### Build query
 query = ""
 for i, c in enumerate(coinTags.values()):
     if i < len(coinTags)-1:
         query += c + " OR "
     else:
         query += c
-query+=" since:2018-05-16 until:2018-05-18"
+query+=" since:"+collectingDataSince+" until:"+collectingDataUntil
 print(len(query))
 #########################################################################33
 
@@ -104,10 +111,10 @@ class TweepyWrapper():
         return users
 
     def search(self,keyword):
-        tweets = self._api.search(q=keyword)
+        tweets = self._api.search(q=keyword, tweet_mode='extended')
         for row in tweets:
             # textoutx.append(row.text)
-            print(row.text)
+            print(row.full_text)
         return tweets
 
     def getRateLimit(self):
@@ -128,7 +135,7 @@ class TweepyWrapper():
         quota = twt._api.rate_limit_status('search')['resources']['search']
         callLeft = str(quota['/search/tweets']['remaining'])
         resetTime = datetime.datetime.fromtimestamp(quota['/search/tweets']['reset']).strftime('%Y-%m-%d %H:%M:%S')
-        print("Quota (call/time): " + callLeft + "/" + resetTime)
+        print("Quota (call/resetAt): " + callLeft + "/" + resetTime)
 
 #######################################################
 # Main
@@ -142,53 +149,115 @@ class TweepyWrapper():
 
 # Initial wrapper
 twt = TweepyWrapper()
-
+# ### Stored fields ##################################################################
+# aa = twt.search("thanadonf")
+# t = aa[0]
+# ## tweet
+# created_at = t.created_at
+# tid = t.id
+# tweet = t.full_text
+# tweet_from_country = t.place.country if t.place is not None else None
+#
+# ## user /
+# t.user.id
+# t.user.screen_name
+# t.user.followers_count
+# t.user.friends_count
+#
+# ## build/update node + edge
+#
+# mentioned_uids = [i['id'] for i in t.entities['user_mentions']]
+#
+# hashtags = [i['text'] for i in t.entities['hashtags']]
+#
+# links = [i['url'] for i in t.entities['urls']]
+#
+# reply_to_tid = t.in_reply_to_status_id
+# reply_to_uid = t.in_reply_to_user_id
+#
+# retweet_from_tid = t.retweeted_status.id #check none
+# retweet_from_uid = t.retweeted_status.user.id #check none
+#
+# #<class 'list'>: [{'url': 'https://t.co/1V9RlGWRfE', 'expanded_url': 'http://www.google.com', 'display_url': 'google.com', 'indices': [46, 69]}, {'url': 'https://t.co/PKRIQSuGxd', 'expanded_url': 'http://www.sanook.com', 'display_url': 'sanook.com', 'indices': [70, 93]}]
+# ##################################################################
 ###############################
 # Loading tweets
 ###############################
 # Initial json file as list
+jsonFormat = {'tweets':[]}
 with open(filename, 'w') as fp:
-    json.dump([], fp, sort_keys=False, indent=4)
+    json.dump(jsonFormat, fp, sort_keys=False, indent=4)
+
+# Function for updating file
+def updateJson(fname, infos):
+    # load previous tweets
+    with open(fname) as f:
+        data = json.load(f)
+    #getList of tweet from dict{tweets:[{info1},{info2},..]
+    twList = data['tweets']
+    # concate new tweets
+    twList += infos
+    data['tweets'] = twList
+    # overwrite new tweets
+    with open(fname, 'w') as f:
+        json.dump(data, f, sort_keys=False, indent=4)
 
 # Start loading with cursor
 page_count = 0
+infos = []
 for tweets in tweepy.Cursor(twt._api.search, q=query, tweet_mode='extended', count=100).pages():
     page_count += 1
     print("processing page: " + str(page_count) + " with " + str(len(tweets)) + " tweets...")
     print("Sample: " + str(tweets[0].created_at))
+
     # build tweet info
-    infos = []
     for t in tweets:
-        if 'retweeted_status' in t._json:
-            t = t.retweeted_status
-        hashtags = []
-        for h in t.entities['hashtags']:
-            hashtags.append(h['text'])
         info = {}
-        info['id'] = t.id
-        info['tweet'] = t.full_text
-        info['user_id'] = t.author.id
+        # if tweet is not original, text will be truncated to 144 length.
+        # Need to retrieve text and hashtags from original tweet.
+        if 'retweeted_status' in t._json:
+            tsource = t.retweeted_status
+            info['hashtags'] = [i['text'] for i in tsource.entities['hashtags']]
+            info['tweet'] = tsource.full_text
+        else: #if its original, retrieve normally
+            info['hashtags'] = [i['text'] for i in t.entities['hashtags']]
+            info['tweet'] = t.full_text
+        #tweet
+        info['tid'] = t.id
         info['created_at'] = str(t.created_at)
-        info['hashtags'] = hashtags
+        info['country'] = t.place.country if t.place is not None else None
+        info['link_count'] = len(t.entities['urls'])
+        #user
+        info['uid'] = t.user.id
+        info['screen_name'] = t.user.screen_name
+        info['followers_count'] = t.user.followers_count
+        info['friends_count'] = t.user.friends_count
+
+        #relationship
+        info['mentioned_uids'] = [i['id'] for i in t.entities['user_mentions']]
+        info['reply_to_tid'] = t.in_reply_to_status_id
+        info['reply_to_uid'] = t.in_reply_to_user_id
+        info['retweet_from_tid'] = info['retweet_from_uid'] = None
+        if 'retweeted_status' in t._json:
+            info['retweet_from_tid'] = t.retweeted_status.id
+            info['retweet_from_uid'] = t.retweeted_status.user.id
+
         infos.append(info)
 
-    # Bulk write data every 50 pages
-    if (page_count%50 == 0):
-        # load previous tweets
-        with open(filename) as f:
-            data = json.load(f)
-        # append with new tweets
-        data += infos
-        # overwrite new tweets
-        with open(filename, 'w') as f:
-            json.dump(data, f, sort_keys=False, indent=4)
-
-        logging.info("Save page: " + str(page_count))
+    # Bulk write data every n pages
+    if (page_count%bulkSize == 0):
+        updateJson(filename, infos)
+        updatedTweetCount = len(infos)
+        logging.info("Save page/updated_twt: " + str(page_count) + "/" + str(updatedTweetCount))
+        infos = []
 
     # Print remaining quota
     twt.printQuota()
+# update again to save last bulk
+updateJson(filename, infos)
+logging.info("Save page/updated_twt: " + str(page_count) + "/" + str(updatedTweetCount))
 
-print('Done..')
+print('Last bulk updated.. Done..')
 
 
 # getTimeline
@@ -196,4 +265,8 @@ print('Done..')
 # for t in tweets:
 #     print(str(t.created_at) + " | " + str(t.retweeted) + ": " + t.text)
 
-
+# # load previous tweets
+# with open("test2018-05-11.json") as f:
+#     data = json.load(f)
+#
+# print(len(data))

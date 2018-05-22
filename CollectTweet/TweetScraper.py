1bdf9af23e270488b383577e00be9853ea832594
kinetiz <thanadon.f@gmail.com>
2018-05-22 15:41:11 +0100

    clean code

--- CollectTweet/TweetScraper.py
+++ CollectTweet/TweetScraper.py
@@ -2,15 +2,99 @@ import tweepy
 import json
 import logging
 import datetime
+
+### Class tweepy wrapper
+class TweepyWrapper():
+    _consumer_key = ""
+    _consumer_secret = ""
+    _access_token = ""
+    _access_token_secret = ""
+    _myinfo =""
+    _api = ""
+
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        self._consumer_key = consumer_key
+        self._consumer_secret = consumer_secret
+        self._access_token = access_token
+        self._access_token_secret = access_token_secret
+
+        # Setup authentication objects
+        __auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        __auth.set_access_token(access_token, access_token_secret)
+        self._api = tweepy.API(__auth,
+                   # support for multiple authentication handlers
+                   # retry 3 times with 5 seconds delay when getting these error codes
+                   # For more details see
+                   # https://dev.twitter.com/docs/error-codes-responses
+                   retry_count=3, retry_delay=5, retry_errors=set([401, 404, 500, 503])
+                    , wait_on_rate_limit=True, wait_on_rate_limit_notify=True
+                   )
+        self._myinfo = self._api.me()
+
+    def sendTweet(self, text):
+        # Update status
+        self._api.update_status(status=text)
+
+    def getUser(self, name):
+        users = self._api.get_user(name)
+        return users
+
+    def search(self,keyword):
+        tweets = self._api.search(q=keyword, tweet_mode='extended')
+        for row in tweets:
+            # textoutx.append(row.text)
+            print(row.full_text)
+        return tweets
+
+    def getRateLimit(self):
+        rate = self._api.rate_limit_status()
+        print(rate['resources']['search'])
+
+    def getTweetByUser(self,userName):
+        tweets = self._api.user_timeline(userName)
+        return tweets
+
+    def getRetweetUsers(self,tweetId):
+        users = self._api.retweets(tweetId)
+        return users
+    def getTweetById(self,tweetId):
+        return self._api.get_status
+    def printQuota(self):
+        # Print remaining quota
+        quota = twt._api.rate_limit_status('search')['resources']['search']
+        callLeft = str(quota['/search/tweets']['remaining'])
+        resetTime = datetime.datetime.fromtimestamp(quota['/search/tweets']['reset']).strftime('%Y-%m-%d %H:%M:%S')
+        print("Quota (call/resetAt): " + callLeft + "/" + resetTime)
+
 ##############################################################
 # Configuration
 ##############################################################
 ### Setup logging
 logging.basicConfig(filename='history.log',level=logging.INFO, format='%(asctime)s.%(msecs)03d : %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
 
+### Setup authentication
+# ## App1
+__CONSUMER_KEY = 'l9BvOKjP2Xj48I5wlc7Uh8sVN'
+__CONSUMER_SECRET = 'HMKOvmuCaReDIzc0GlzfjQGKFrdCJ9jHlRJd8AAp0e7PHfwl6H'
+__ACCESS_TOKEN = '982717247109189632-cdUZCv2mzMCDiPiHkdRDpUQXdRnFIvB'
+__ACCESS_TOKEN_SECRET = 'AaayN6WOC3rPcYIoBg1Cc5uEPeUbcrw2jFHhhZVlHzjGC'
+
+# ### App2
+# __CONSUMER_KEY = 'L7IX0KwYgQUGeC1TqnxULue1v'
+# __CONSUMER_SECRET = 'gRiqmdED0KFVjg6v2x9giQmctFLdAiFwEioCECuVCTJafp8byB'
+# __ACCESS_TOKEN = '982717247109189632-QRXdgrmZdhIy00do6D5o0wpdUMVKvJU'
+# __ACCESS_TOKEN_SECRET = 'szb7jQaVBe8JCf9iM23e3GBd7OgbuLRDxhTM6nU4L3QAp'
+
+# ## App3
+# __CONSUMER_KEY = 'w3TysRbh9H6oKp5T8qVNeSEdl'
+# __CONSUMER_SECRET = 'LJA4sjmh4mq7OvmfUngVr7OX7rAnxeuUsKPT9rbzD9iU1sWfa3'
+# __ACCESS_TOKEN = '982717247109189632-rB4KizgqhsFdMS1V1Tu8qcPdxAe2dDi'
+# __ACCESS_TOKEN_SECRET = 'joe3MY7vIjWhDttHMMTJmgchzOLYUNqhjGUuWpAS4d3Ro'
+####
+
 ### Collecting date range
-collectingDataSince = "2018-05-12"
-collectingDataUntil = "2018-05-16"
+collectingDataSince = "2018-05-13"
+collectingDataUntil = "2018-05-14"
 
 ### intial json file name
 filename = 'top1_'+collectingDataSince+'_to_'+collectingDataUntil+'.json'
@@ -20,7 +104,7 @@ bulkSize = 50
 
 ### Define query
 coinTags = {}
-### 1 ###
+### 1
 coinTags['bitcoin'] = "#bitcoin OR #btc"
 
 ### 2-10 ###
@@ -57,6 +141,7 @@ coinTags['bitcoin'] = "#bitcoin OR #btc"
 # coinTags['steem'] = "#steem"
 
 ### Build query
+
 query = ""
 for i, c in enumerate(coinTags.values()):
     if i < len(coinTags)-1:
@@ -65,121 +150,19 @@ for i, c in enumerate(coinTags.values()):
         query += c
 query+=" since:"+collectingDataSince+" until:"+collectingDataUntil
 print(len(query))
-#########################################################################33
+#########################################################################
 
-
-### Class tweepy wrapper
-class TweepyWrapper():
-    # Variables for authentication
-    ## App1
-    __CONSUMER_KEY = 'l9BvOKjP2Xj48I5wlc7Uh8sVN'
-    __CONSUMER_SECRET = 'HMKOvmuCaReDIzc0GlzfjQGKFrdCJ9jHlRJd8AAp0e7PHfwl6H'
-    __ACCESS_TOKEN = '982717247109189632-cdUZCv2mzMCDiPiHkdRDpUQXdRnFIvB'
-    __ACCESS_TOKEN_SECRET = 'AaayN6WOC3rPcYIoBg1Cc5uEPeUbcrw2jFHhhZVlHzjGC'
-
-    # ## App2
-    # __CONSUMER_KEY = 'L7IX0KwYgQUGeC1TqnxULue1v'
-    # __CONSUMER_SECRET = 'gRiqmdED0KFVjg6v2x9giQmctFLdAiFwEioCECuVCTJafp8byB'
-    # __ACCESS_TOKEN = '982717247109189632-QRXdgrmZdhIy00do6D5o0wpdUMVKvJU'
-    # __ACCESS_TOKEN_SECRET = 'szb7jQaVBe8JCf9iM23e3GBd7OgbuLRDxhTM6nU4L3QAp'
-
-    # ## App3
-    # __CONSUMER_KEY = 'w3TysRbh9H6oKp5T8qVNeSEdl'
-    # __CONSUMER_SECRET = 'LJA4sjmh4mq7OvmfUngVr7OX7rAnxeuUsKPT9rbzD9iU1sWfa3'
-    # __ACCESS_TOKEN = '982717247109189632-rB4KizgqhsFdMS1V1Tu8qcPdxAe2dDi'
-    # __ACCESS_TOKEN_SECRET = 'joe3MY7vIjWhDttHMMTJmgchzOLYUNqhjGUuWpAS4d3Ro'
-
-    # Setup authentication objects
-    __auth = tweepy.OAuthHandler(__CONSUMER_KEY, __CONSUMER_SECRET)
-    __auth.set_access_token(__ACCESS_TOKEN, __ACCESS_TOKEN_SECRET)
-    _api = tweepy.API(__auth,
-               # support for multiple authentication handlers
-               # retry 3 times with 5 seconds delay when getting these error codes
-               # For more details see
-               # https://dev.twitter.com/docs/error-codes-responses
-               retry_count=3, retry_delay=5, retry_errors=set([401, 404, 500, 503])
-                , wait_on_rate_limit=True, wait_on_rate_limit_notify=True
-               )
-    _myinfo = _api.me()
-
-    def sendTweet(self, text):
-        # Update status
-        self._api.update_status(status=text)
-
-    def getUser(self, name):
-        users = self._api.get_user("Blockchain")
-        return users
-
-    def search(self,keyword):
-        tweets = self._api.search(q=keyword, tweet_mode='extended')
-        for row in tweets:
-            # textoutx.append(row.text)
-            print(row.full_text)
-        return tweets
-
-    def getRateLimit(self):
-        rate = self._api.rate_limit_status()
-        print(rate['resources']['search'])
-
-    def getTweetByUser(self,userName):
-        tweets = self._api.user_timeline(userName)
-        return tweets
-
-    def getRetweetUsers(self,tweetId):
-        users = self._api.retweets(tweetId)
-        return users
-    def getTweetById(self,tweetId):
-        return self._api.get_status
-    def printQuota(self):
-        # Print remaining quota
-        quota = twt._api.rate_limit_status('search')['resources']['search']
-        callLeft = str(quota['/search/tweets']['remaining'])
-        resetTime = datetime.datetime.fromtimestamp(quota['/search/tweets']['reset']).strftime('%Y-%m-%d %H:%M:%S')
-        print("Quota (call/resetAt): " + callLeft + "/" + resetTime)
 
 #######################################################
 # Main
 #######################################################
 # Note
-# - if retweeted = true, keep only the original tweet
-# - use cursor to manage limit + loop running
 # - save to neo4j
-# - define more keywords
 # - analyse network to refine keywords
 
 # Initial wrapper
-twt = TweepyWrapper()
-# ### Stored fields ##################################################################
-# aa = twt.search("thanadonf")
-# t = aa[0]
-# ## tweet
-# created_at = t.created_at
-# tid = t.id
-# tweet = t.full_text
-# tweet_from_country = t.place.country if t.place is not None else None
-#
-# ## user /
-# t.user.id
-# t.user.screen_name
-# t.user.followers_count
-# t.user.friends_count
-#
-# ## build/update node + edge
-#
-# mentioned_uids = [i['id'] for i in t.entities['user_mentions']]
-#
-# hashtags = [i['text'] for i in t.entities['hashtags']]
-#
-# links = [i['url'] for i in t.entities['urls']]
-#
-# reply_to_tid = t.in_reply_to_status_id
-# reply_to_uid = t.in_reply_to_user_id
-#
-# retweet_from_tid = t.retweeted_status.id #check none
-# retweet_from_uid = t.retweeted_status.user.id #check none
-#
-# #<class 'list'>: [{'url': 'https://t.co/1V9RlGWRfE', 'expanded_url': 'http://www.google.com', 'display_url': 'google.com', 'indices': [46, 69]}, {'url': 'https://t.co/PKRIQSuGxd', 'expanded_url': 'http://www.sanook.com', 'display_url': 'sanook.com', 'indices': [70, 93]}]
-# ##################################################################
+twt = TweepyWrapper(__CONSUMER_KEY,__CONSUMER_SECRET,__ACCESS_TOKEN,__ACCESS_TOKEN_SECRET)
+
 ###############################
 # Loading tweets
 ###############################
@@ -227,6 +210,7 @@ for tweets in tweepy.Cursor(twt._api.search, q=query, tweet_mode='extended', cou
         info['created_at'] = str(t.created_at)
         info['country'] = t.place.country if t.place is not None else None
         info['link_count'] = len(t.entities['urls'])
+
         #user
         info['uid'] = t.user.id
         info['screen_name'] = t.user.screen_name
